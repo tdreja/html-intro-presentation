1b36ed7621cc3bf8fd912318ab1377b4e372902c
Thomas Dreja <thomas.dreja@exxcellent.de>
2025-02-09 16:34:01 +0100

    chore(project): Add Spring Initializr project

---
+++ src/main/java/de/dreja/introgenerator/HtmlIntroGeneratorApplication.java
@@ -0,0 +1,13 @@
+package de.dreja.introgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmlIntroGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmlIntroGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/dreja/introgenerator/HtmlIntroGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dreja.introgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlIntroGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
