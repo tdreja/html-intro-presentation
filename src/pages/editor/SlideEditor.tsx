c92ee86e0f372b708267485b0910fb224fea89b5
Thomas Dreja <thomas.dreja@exxcellent.de>
2026-05-05 15:28:30 +0200

    feat(Editor): Add quill

--- src/pages/editor/SlideEditor.tsx
+++ src/pages/editor/SlideEditor.tsx
@@ -1,13 +1,45 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
 import { EditorProps } from './EditorProps.ts';
+import ReactQuill from 'react-quill-new';
+import 'react-quill-new/dist/quill.snow.css';
+import { SlideId } from '../../model/Slide.ts';
+import { UpdateSlideContentEvent } from '../../model/ChangeEvent.ts';
 
-export const SlideEditor = ({}: EditorProps): ReactElement => {
+export const SlideEditor = ({ editedSlideShow, editedSlideId, onAddChange }: EditorProps): ReactElement => {
+    const [slideId, setSlideId] = useState<SlideId | null>(editedSlideId);
+    const [slideContent, setSlideContent] = useState<string>('');
+
+    useEffect(() => {
+        if (slideId !== editedSlideId) {
+            setSlideId(editedSlideId);
+            const editedSlide = editedSlideShow.slides.find((slide) => slide.id === editedSlideId);
+            setSlideContent(editedSlide ? editedSlide.content.value : '');
+        }
+    }, [slideId, editedSlideId]);
+
     return (
-        <div id="slide-editor">
-            <label htmlFor="slide-content">Slide 1 – Inhalt</label>
-            <div id="slide-16-9-wrap">
-                <textarea id="slide-content" className="form-control" placeholder="Slide-Inhalt hier eingeben…&#10;&#10;Unterstützt Markdown, HTML oder Plain Text."></textarea>
-            </div>
+        <div id="slide-editor-wrapper">
+            <ReactQuill
+                theme="snow"
+                value={slideContent}
+                onChange={setSlideContent}
+                modules={{
+                    toolbar: [
+                        [{ header: [1, 2, false] }],
+                        ['bold', 'italic', 'underline'],
+                        ['image', 'code-block'],
+                    ],
+                }}
+                onBlur={() => {
+                    if (!slideId) {
+                        return;
+                    }
+                    const slide = editedSlideShow.slides.find((slide) => slide.id === slideId);
+                    if (slide && slide.content.value !== slideContent) {
+                        onAddChange(new UpdateSlideContentEvent(slideId, slideContent));
+                    }
+                }}
+            />
         </div>
     );
 };
