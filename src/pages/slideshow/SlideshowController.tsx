8ef419b023cbe9259e233e086ae808e8af4f3db1
Thomas Dreja <thomas.dreja@exxcellent.de>
2026-05-07 13:52:14 +0200

    feat(Slideshow): Add back to editor button

--- src/pages/slideshow/SlideshowController.tsx
+++ src/pages/slideshow/SlideshowController.tsx
@@ -1,15 +1,19 @@
-import React, { ReactElement, useContext } from 'react';
-import { Carousel } from 'react-bootstrap';
+import React, { ReactElement, useContext, useState } from 'react';
+import { Button, Carousel } from 'react-bootstrap';
 import './slideshow-style.css';
 import { SlideshowContext } from '../../component/SlideshowContext.ts';
 import { DisplayHtml } from '../../component/DisplayHtml.tsx';
 import { Countdown } from './Countdown.tsx';
+import { RouteContext } from '../../component/RouteContext.ts';
+import { Route } from '../../model/Route.ts';
 
 export const SlideshowController = (): ReactElement => {
     const [slideshow] = useContext(SlideshowContext);
+    const [_, setRoute] = useContext(RouteContext);
+    const [slideIndex, setSlideIndex] = useState<number>(0);
     return (
         <div id="slideshow-controller">
-            <Carousel controls={false} interval={4000} wrap className="m-3">
+            <Carousel activeIndex={slideIndex} onSelect={setSlideIndex} indicators={false} wrap className="m-3">
                 {slideshow.slides.map((slide) => (
                     <Carousel.Item key={slide.id}>
                         <DisplayHtml html={slide.content} />
@@ -17,6 +21,9 @@ export const SlideshowController = (): ReactElement => {
                 ))}
             </Carousel>
             <div id="slideshow-bottom">
+                <Button variant="outline-primary" id="to-editor-button" onClick={() => setRoute(Route.EDITOR)}>
+                    <span className="material-symbols-outlined">dashboard_customize</span>
+                </Button>
                 <Countdown />
             </div>
         </div>
